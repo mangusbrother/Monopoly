6c96ab329fbf85a017db5c733c7fdb48b2344d65
Samantha Catania <samanthacatania@gmail.com>
2016-01-15 21:39:09 +0100

    InsufficientFundsException

---
+++ monopoly/src/main/java/com/mangusbrother/InsufficientFundsException.java
@@ -0,0 +1,9 @@
+package com.mangusbrother;
+
+/**
+ * Created by samanthacatania on 15/01/2016.
+ */
+public class InsufficientFundsException extends Throwable {
+    public InsufficientFundsException(double v) {
+    }
+}
