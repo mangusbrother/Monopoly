62598b27553a263dbfff412b610fdb903937d054
Samantha Catania <samanthacatania@gmail.com>
2016-01-15 21:20:17 +0100

    Project Setup

---
+++ monopoly/src/main/java/com/mangusbrother/MonopolyApplication.java
@@ -0,0 +1,12 @@
+package com.mangusbrother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonopolyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonopolyApplication.class, args);
+    }
+}
---
+++ monopoly/src/test/java/com/mangusbrother/MonopolyApplicationTests.java
@@ -0,0 +1,18 @@
+package com.mangusbrother;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MonopolyApplication.class)
+@WebAppConfiguration
+public class MonopolyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
